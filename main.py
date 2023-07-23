5db4a64656fb86ba62f9cdf72949464d90f609b1
osulaimankhail <osulaimankhail@hof-university.de>
2023-07-23 11:21:33 +0200

    bootstrap files are sourced

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from fastapi import FastAPI
 from pymongo import MongoClient
 from src.controller import index_controller
 import uvicorn
+from fastapi import Request, APIRouter
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+
 
 app = FastAPI()
+app.mount("/templates", StaticFiles(directory="src/templates"), name="static")
 
 
 @app.on_event("startup")
@@ -16,13 +23,27 @@ def startup_db_client():
 def shutdown_db_client():
     pass
 
-app.include_router(index_controller.router, tags=["index"], prefix="/index")
+app.include_router(index_controller.router, tags=["index"], prefix="")
 
 
 @app.get("/healthcheck/")
 def healthcheck():
     return 'Health - OK'
 
+
+
+
+router = APIRouter()
+
+#templates = Jinja2Templates(directory="src/templates")
+
+
+#@app.get("/", response_class=HTMLResponse)
+#async def index(request: Request):
+ #   countries = ['Afghanistan', 'USA', 'Russia']
+
+  #  return templates.TemplateResponse("index.html", {"request": request, "countries": countries})
+   # return FileResponse("src/templates/index.html")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- src/controller/index_controller.py
+++ src/controller/index_controller.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Request, APIRouter
 from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
-
+templates = Jinja2Templates(directory="src/templates")
 @router.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return FileResponse("src/templates/index.html")
-    #a
+    #return FileResponse("src/templates/index.html")
+    countries = ['Afghanistan', 'USA', 'Russia']
+    return templates.TemplateResponse("index.html", {"request": request, "countries": countries})
